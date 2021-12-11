ce0ebb98f43b92acf860d921bfa2e4cae4754097
Brianna Groden <briannaggroden@gmail.com>
2021-12-10 23:22:08 -0500

    fixing index of products

---
+++ db/migrate/20211211041556_add_hanger_index_to_products.rb
@@ -0,0 +1,5 @@
+class AddHangerIndexToProducts < ActiveRecord::Migration[6.1]
+  def change
+    add_index :products, :hanger
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_015341) do
+ActiveRecord::Schema.define(version: 2021_12_11_041556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_12_11_015341) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "hanger"
+    t.index ["hanger"], name: "index_products_on_hanger"
   end
 
   create_table "reviews", force: :cascade do |t|
