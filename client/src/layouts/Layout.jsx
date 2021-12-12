ec20155e0923aa6cf2f43731f114803970d596ae
Brianna Groden <briannaggroden@gmail.com>
2021-12-11 20:08:47 -0500

    frontend setup

---
+++ client/src/layouts/Layout.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+export default function Layout(props) {
+  return (
+    <div>
+      <header>
+        <h1>Lets Get Knotty</h1>
+        <h2>macramé</h2>
+        <Link>Sign In</Link>
+      </header>
+      {props.children}
+    </div>
+  )
+}
