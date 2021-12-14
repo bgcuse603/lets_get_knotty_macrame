430c6bc4a665c9102f35475a7c79f3b8ebc9451d
Brianna Groden <briannaggroden@gmail.com>
2021-12-14 15:46:13 -0500

    header css

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
   return (
     <div className='nav'>
-      <Link to='/home'>Home</Link>
+      <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       <Link to='/products'>Products</Link>
       <Link to='/contact'>Contact</Link>
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,24 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Nav from '../components/Nav';
+import '../assets/css/Layout.css';
 
 export default function Layout({ currentUser, handleLogout, children }) {
+  const location = useLocation();
   
   return (
     <div>
       <header>
-        <h1>Lets Get Knotty</h1>
-        <h2>macramé</h2>
-        { currentUser ? (
-          <div>
-            <p>Hi, {`${currentUser.username}`}</p>
-            <button onClick={handleLogout}>Logout</button>
-            {/* <Link to='/register'>
-              <button>Register New User</button>
-            </Link> */}
-          </div>
-          ) : (
-            <Link to='/signin'>Sign In</Link>
-        )}
+        <div className='mainHeader'>
+          {location.pathname === "/" ? (
+            <div> </div>) :
+            (<div className='mainHeaderText'>
+              <h1 className='zeyada'>Lets Get Knotty...  </h1>
+              <h2 className='raleway'>MACRAMÉ</h2>
+              </div>)}
+          { currentUser ? (
+            <div>
+              <p>Hi, {`${currentUser.username}`}</p>
+              <button onClick={handleLogout}>Logout</button>
+              {/* <Link to='/register'>
+                <button>Register New User</button>
+              </Link> */}
+            </div>
+            ) : (
+              <Link to='/signin'>Sign In</Link>
+          )}
+        </div>
+        <div className='subheader'>
+          {location.pathname === "/" ? (<img src='https://i.imgur.com/VyBhgBU.jpg' height='500' />) : (<img src=''/>)}
+        </div>
       </header>
       <section>
         <Nav />
