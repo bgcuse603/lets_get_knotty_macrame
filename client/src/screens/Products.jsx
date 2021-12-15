cc26754f1869169e0997882e650596917848dc15
Brianna Groden <briannaggroden@gmail.com>
2021-12-15 11:15:27 -0500

    products page

--- client/src/screens/Products.jsx
+++ client/src/screens/Products.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import '../assets/css/Products.css';
 import { Link } from 'react-router-dom';
 
 export default function Products({ products }) {
-
+  const productsLinkStyle = {
+    color: "#bc7554",
+    textDecoration: "none",
+    fontFamily: "raleway",
+    padding: "4vh",
+  
+  };
+  
   return (
     <div>
-      <div>
-        <h3>PRODUCTS</h3>
+      <div className='productsMain'>
+        <h1>Products</h1>
         <Link to={`/newproduct`}>
           <button>CREATE NEW</button>
         </Link>
       </div>
-      <div>
+      <div className='productsBody'>
       {products.map((product, index) => (
-        <Link to={`/products/${product.id}`} key = {index}>
+        <Link to={`/products/${product.id}`} style={ productsLinkStyle} key = {index}>
             <div className="productDiv" >
             <img src={`${product.img}`} alt="macrame" width="300" height="300"></img>
-            <h4>{`${product.item}`}</h4>
+            <h2>Item {`${product.item}`}</h2>
           </div>
         </Link>
       ))}
