bc885701b0f485e3b0ea7a3de4744c110fc665f3
Brianna Groden <briannaggroden@gmail.com>
2021-12-15 11:00:04 -0500

    home page links

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,4 +1,5 @@
 import '../assets/css/Home.css';
+import { Link } from 'react-router-dom';
 import iconEarring from '../assets/images/iconEarring.png';
 import iconPlantHanger from '../assets/images/iconPlantHanger.png';
 import iconWallHanger from '../assets/images/iconWallHanger.png';
@@ -8,15 +9,21 @@ export default function Home() {
   return (
     <div className='homeDiv'>
       <div className='homeIconBlock'>
-        <img src={iconWallHanger} height='200vh'/>
+        <Link to='products'>
+          <img src={iconWallHanger} height='200vh'/>
+        </Link>
         <h1>Wall Hangers</h1>
       </div>
       <div className='homeIconBlock'>
-        <img src={iconPlantHanger} height='200vh' />
+        <Link to='products'>
+          <img src={iconPlantHanger} height='200vh' />
+        </Link>
         <h1>Plant Hangers</h1>
       </div>
       <div className='homeIconBlock'>
-        <img src={iconEarring} height='200vh' />
+        <Link to='products'>
+          <img src={iconEarring} height='200vh' />
+        </Link>
         <h1>Earrings</h1>
       </div>
     </div>
